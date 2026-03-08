f639239c1f2d0afb06a4993c25d3c7273d666f5f
Aryan Singh <getaryanonline@gmail.com>
2026-03-08 14:20:28 -0700

    phase 3

--- logic_utils.py
+++ logic_utils.py
@@ -3,10 +3,9 @@ def get_range_for_difficulty(difficulty: str):
     if difficulty == "Easy":
         return 1, 20
     if difficulty == "Normal":
-        return 1, 100
-    if difficulty == "Hard":
         return 1, 50
-    return 1, 100
+    if difficulty == "Hard":
+        return 1, 100
 
 
 def parse_guess(raw: str):
